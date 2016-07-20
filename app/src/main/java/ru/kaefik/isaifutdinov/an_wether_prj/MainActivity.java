66e60209ce7fff87ec76c24cb08f12742305fb48
Ilnur Saifutdinov <ilnursoft@gmail.com>
2016-07-20 11:34:44 +0300

    add getHttpRequestFromUrl

--- app/src/main/java/ru/kaefik/isaifutdinov/an_wether_prj/MainActivity.java
+++ app/src/main/java/ru/kaefik/isaifutdinov/an_wether_prj/MainActivity.java
@@ -1,13 +1,10 @@
 package ru.kaefik.isaifutdinov.an_wether_prj;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
-import org.json.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,7 +13,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,17 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView nameCity;
     private TextView myText;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameCity =  (ListView) findViewById(R.id.listView);
-        myText =  (TextView) findViewById(R.id.textView);
+        nameCity = (ListView) findViewById(R.id.listView);
+        myText = (TextView) findViewById(R.id.textView);
 
-        CityModelAdapter adapter = new CityModelAdapter(this,initDataCity());
+        CityModelAdapter adapter = new CityModelAdapter(this, initDataCity());
 
         nameCity.setAdapter(adapter);
 
@@ -50,31 +46,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-// инициализация данных для списка городов
-    private List<CityModel> initDataCity(){
+    // инициализация данных для списка городов
+    private List<CityModel> initDataCity() {
         List<CityModel> listdata = new ArrayList<CityModel>();
 
-        listdata.add(new CityModel(1,"ru","Kazan",0,0,0,0,0,0,0));
-        listdata.add(new CityModel(2,"ru","Moscow",0,0,0,0,0,0,0));
-        listdata.add(new CityModel(3,"ru","Samara",0,0,0,0,0,0,0));
-        listdata.add(new CityModel(4,"tr","Istanbul",0,0,0,0,0,0,0));
-        listdata.add(new CityModel(5,"gb","City of London",0,0,0,0,0,0,0));
+        listdata.add(new CityModel(1, "ru", "Kazan", 0, 0, 0, 0, 0, 0, 0));
+        listdata.add(new CityModel(2, "ru", "Moscow", 0, 0, 0, 0, 0, 0, 0));
+        listdata.add(new CityModel(3, "ru", "Samara", 0, 0, 0, 0, 0, 0, 0));
+        listdata.add(new CityModel(4, "tr", "Istanbul", 0, 0, 0, 0, 0, 0, 0));
+        listdata.add(new CityModel(5, "gb", "City of London", 0, 0, 0, 0, 0, 0, 0));
 
-        return  listdata;
+        return listdata;
     }
 
-// ?????
-    public void clickItemCity(View view,int position,long id){
-        myText.setText( "itemClick: position = " + position + ", id = " + id );
+    // ?????
+    public void clickItemCity(View view, int position, long id) {
+        myText.setText("itemClick: position = " + position + ", id = " + id);
     }
 
     // получить текущую погоду из OpenWeatherMap используя id города
-    public CityModel  getCurrentWeather(long id){
-        CityModel res = new CityModel(id,"","",0,0,0,0,0,0,0);
-        String strid ="524901";
-        String strurl=  "http://api.openweathermap.org/data/2.5/forecast/city?id="+strid+"&APPID=9a4be4eeb7de3b88211989559a0849f7";
+    public CityModel getCurrentWeather(long id) {
+        CityModel res = new CityModel(id, "", "", 0, 0, 0, 0, 0, 0, 0);
+        String strid = "524901";
+        String strurl = "http://api.openweathermap.org/data/2.5/forecast/city?id=" + strid + "&APPID=9a4be4eeb7de3b88211989559a0849f7";
 //        myText.setText(getHTML(strurl));
-        return  res;
+        return res;
     }
 
     public void getInfoWeatherCity(View view) throws Exception {
@@ -103,73 +99,44 @@ public class MainActivity extends AppCompatActivity {
 //        startActivity(intent);
     }
 
-    public void getHttpWeather() throws MalformedURLException {
+    public void getHttpWeather()  {
 
-//        String url=  "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=9a4be4eeb7de3b88211989559a0849f7";
+        //api.openweathermap.org/data/2.5/weather?q=London&APPID=9a4be4eeb7de3b88211989559a0849f7
 
-        HttpURLConnection connection=null;
-        BufferedReader reader=null;
-        URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=9a4be4eeb7de3b88211989559a0849f7");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getHttpRequestFromUrl("http://api.openweathermap.org/data/2.5/weather?q=London&APPID=9a4be4eeb7de3b88211989559a0849f7");
 
-        try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-
-            InputStream stream = connection.getInputStream();
-
-            StringBuffer buffer = new StringBuffer();
-
-            reader = new BufferedReader(new InputStreamReader(stream));
-
-            String line = "";
-            while (((line = reader.readLine()) != null)) {
-                buffer.append(line);
             }
+        }).start();
 
-            myText.setText(line);
-        } catch (MalformedURLException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if(connection != null) {
-                connection.disconnect();
-            }
-            try {
-                if(reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-
-
-//        JsonObjectRequest jsObjRequest = new JsonObjectRequest
-//                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-//
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        myText.setText("Response: " + response.toString());
-//                    }
-//                }, new Response.ErrorListener() {
-//
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        // TODO Auto-generated method stub
-//
-//                    }
-//                });
-//
+//        myText.setText(finalJson);
 
     }
 
-
-
-
-
-
+    // получение страницы из урла strurl
+    public String getHttpRequestFromUrl(String strurl){
+        try {
+            URL url = new URL(strurl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
+            InputStream stream = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(stream);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+            return line;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
